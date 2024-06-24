300674088c5db6b696de96f2152df03a3ba9bb22
Blake Azuela <blake@azuela.info>
2024-06-24 14:00:10 -0400

    Adding Django Base Files: - Added initial Django Files and Folders - Added TimeMachine

---
+++ TimeMachineSite/TimeMachineApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TimemachineappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TimeMachineApp'
---
+++ TimeMachineSite/TimeMachineApp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+'''
+class Topic(models.Model):
+    top_name = models.CharField(max_length=264, unique=True)
+
+class Webpage(models.Model):
+    category = models.ForeignKey(Topic)
+    name = models.CharField(max_length=264)
+    url = models.URLField()
+    
+    
+    def __str__(self):
+        return self.name
+'''
